18a81048779bc2ee35a8b3fb5aa684e629f5ae9f
Janko Krstic <keichiri@protonmail.com>
2018-06-02 18:27:31 +0200

    file composing implementation

--- storage/storage.go
+++ storage/storage.go
@@ -1,11 +1,16 @@
 package storage
 
 import (
+	"errors"
 	"fmt"
+	"gobby"
 	"gobby/logs"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -108,6 +113,93 @@ func (dh *DirectoryHandler) retrieveAndSendPiece(index int) {
 	dh.requestsMx.Unlock()
 }
 
+// Blocking operation, intended to be executed once per download process
+// Assumes clean directory
+// TODO - support continuation between different invocations and preparation if needed
+func (dh *DirectoryHandler) ComposeFiles(fileInfos []*gobby.File) error {
+	indexes, err := listExistingIndexes(dh.piecesPath)
+	if err != nil {
+		return fmt.Errorf("Failed to list existing indexes: %s", err)
+	}
+
+	for i := 0; i < len(indexes)-1; i++ {
+		if indexes[i+1]-indexes[i] != 1 {
+			return errors.New("Cannot compose files yet. Missing indexes")
+		}
+	}
+
+	err = dh.populateFilesFromIndexes(fileInfos, indexes)
+	if err != nil {
+		cleanupFiles(dh.path)
+		return fmt.Errorf("Failed to compose files: %s", err)
+	}
+
+	return nil
+}
+
+func (dh *DirectoryHandler) populateFilesFromIndexes(fileInfos []*gobby.File, indexes []int) error {
+	var currentPieceIndex int
+	var currentPieceOffset int
+
+	for _, fileInfo := range fileInfos {
+		fullPath := filepath.Join(dh.path, fileInfo.Path)
+		directory := filepath.Dir(fullPath)
+		err := os.MkdirAll(directory, 0700)
+		if err != nil {
+			return fmt.Errorf("Failed to create directory at path %s. Error: %s", directory, err)
+		}
+
+		f, err := os.Create(fullPath)
+		if err != nil {
+			return fmt.Errorf("Failed to create file at path: %s. Error: %s", fullPath, err)
+		}
+
+		toWrite := fileInfo.Length
+
+		for toWrite > 0 {
+			if currentPieceIndex >= len(indexes) {
+				return errors.New("Failed to populate all files based on available pieces")
+			}
+
+			piecePath := createPiecePath(dh.piecesPath, currentPieceIndex)
+			pieceFile, err := os.Open(piecePath)
+			if err != nil {
+				return fmt.Errorf("Failed to open piece file: %s", err)
+			}
+
+			pieceData, err := ioutil.ReadAll(pieceFile)
+			if err != nil {
+				return fmt.Errorf("Failed to read piece data: %s", err)
+			}
+			pieceFile.Close()
+
+			pieceData = pieceData[currentPieceOffset:]
+
+			if len(pieceData) <= toWrite {
+				_, err = f.Write(pieceData)
+				if err != nil {
+					return fmt.Errorf("Failed to write to file: %s", err)
+				}
+				toWrite -= len(pieceData)
+				currentPieceIndex += 1
+				currentPieceOffset = 0
+			} else {
+				_, err = f.Write(pieceData[:toWrite])
+				if err != nil {
+					return fmt.Errorf("Failed to write to file: %s", err)
+				}
+				currentPieceOffset += toWrite
+				toWrite = 0
+			}
+		}
+
+		logs.Info("Storage", "Created %s", fullPath)
+		f.Close()
+	}
+
+	return nil
+}
+
 type StoreResult struct {
 	Index int
 	Err   error
@@ -153,4 +245,58 @@ func retrievePiece(piecesDirPath string, pieceIndex int) ([]byte, error) {
 
 func createPiecePath(piecesDirPath string, pieceIndex int) string {
 	return filepath.Join(piecesDirPath, fmt.Sprintf("%d.piece", pieceIndex))
+}
+
+func listExistingIndexes(piecesDirPath string) ([]int, error) {
+	stats, err := ioutil.ReadDir(piecesDirPath)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to stat pieces directory: %s", err)
+	}
+
+	indexes := make([]int, 0)
+	for _, stat := range stats {
+		if !stat.IsDir() {
+			name := stat.Name()
+			if strings.HasSuffix(name, ".piece") {
+				name = strings.TrimSuffix(name, ".piece")
+				index, err := strconv.Atoi(name)
+				if err == nil {
+					indexes = append(indexes, index)
+				}
+			}
+		}
+	}
+
+	sort.SliceStable(indexes, func(i int, j int) bool {
+		return indexes[i] <= indexes[j]
+	})
+
+	return indexes, nil
+}
+
+func cleanupFiles(path string) {
+	infos, err := ioutil.ReadDir(path)
+	if err != nil {
+		logs.Error("Storage", "Failed to stat %s for cleanup. Error: %s", path, err)
+	}
+
+	for _, info := range infos {
+		if !info.IsDir() {
+			fullPath := filepath.Join(path, info.Name())
+			err := os.Remove(fullPath)
+			if err != nil {
+				logs.Error("Storage", "Failed to delete %s. Error: %s", fullPath, err)
+			}
+		} else {
+			if info.Name() == ".pieces" {
+				continue
+			}
+
+			dirPath := filepath.Join(path, info.Name())
+			err := os.RemoveAll(dirPath)
+			if err != nil {
+				logs.Error("Storage", "Failed to delete %s. Error: %s", dirPath, err)
+			}
+		}
+	}
 }
--- storage/storage_test.go
+++ storage/storage_test.go
@@ -3,8 +3,10 @@ package storage
 import (
 	"bytes"
 	"fmt"
+	"gobby"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -16,7 +18,7 @@ func createHandler() *DirectoryHandler {
 	os.MkdirAll(path, 0700)
 	handler, err := NewDirectoryHandler(path)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to create directoy handler: %s", err))
+		panic(fmt.Sprintf("Failed to create directory handler: %s", err))
 	}
 	return handler
 }
@@ -72,6 +74,91 @@ func TestRetrievePiece(t *testing.T) {
 
 	if !bytes.Equal(data, res.Data) {
 		t.Fatalf("Expected %s. Got %s", string(data), string(res.Data))
+	}
+}
+
+func TestComposeFiles(t *testing.T) {
+	fileInfos := []*gobby.File{
+		&gobby.File{Length: 7, Path: "dir0/file0.txt"},
+		&gobby.File{Length: 5, Path: "/dir0/file1.txt"},
+		&gobby.File{Length: 28, Path: "file2.txt"},
+		&gobby.File{Length: 10, Path: "file3.txt"},
+		&gobby.File{Length: 1, Path: "file4.txt"},
+		&gobby.File{Length: 5, Path: "dir1/file5.txt"},
+	}
+
+	handler := createHandler()
+	data0 := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	data1 := []byte{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	data2 := []byte{2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+	data3 := []byte{3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
+	data4 := []byte{4, 4, 4, 4, 4, 4, 4, 4, 4, 4}
+	data5 := []byte{5, 5, 5, 5, 5, 5}
+	pieceDatas := [][]byte{data0, data1, data2, data3, data4, data5}
+
+	for i, data := range pieceDatas {
+		if err := ioutil.WriteFile(path+fmt.Sprintf("/pieces/%d.piece", i), data, 0700); err != nil {
+			t.Fatalf("Failed to prepare piece: %s", err)
+		}
+	}
+
+	err := handler.ComposeFiles(fileInfos)
+	if err != nil {
+		t.Fatalf("Failed to compose files: %s", err)
+	}
+
+	file0Content, err := ioutil.ReadFile(filepath.Join(path, "/dir0/file0.txt"))
+	if err != nil {
+		t.Fatalf("Failed to read file content. Err: %s", err)
+	}
+	expectedFile0Content := []byte{0, 0, 0, 0, 0, 0, 0}
+	if !bytes.Equal(file0Content, expectedFile0Content) {
+		t.Fatalf("Expected file 0 content: %v. Got: %v", expectedFile0Content, file0Content)
+	}
+
+	file1Content, err := ioutil.ReadFile(filepath.Join(path, "/dir0/file1.txt"))
+	if err != nil {
+		t.Fatalf("Failed to read file content. Err: %s", err)
+	}
+	expectedFile1Content := []byte{0, 0, 0, 1, 1}
+	if !bytes.Equal(file1Content, expectedFile1Content) {
+		t.Fatalf("Expected file 1 content: %v. Got: %v", expectedFile1Content, file1Content)
+	}
+
+	file2Content, err := ioutil.ReadFile(filepath.Join(path, "file2.txt"))
+	if err != nil {
+		t.Fatalf("Failed to read file content. Err: %s", err)
+	}
+	expectedFile2Content := []byte{1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
+	if !bytes.Equal(file2Content, expectedFile2Content) {
+		t.Fatalf("Expected file 2 content: %v. Got: %v", expectedFile2Content, file2Content)
+	}
+
+	file3Content, err := ioutil.ReadFile(filepath.Join(path, "file3.txt"))
+	if err != nil {
+		t.Fatalf("Failed to read file content. Err: %s", err)
+	}
+	expectedFile3Content := []byte{4, 4, 4, 4, 4, 4, 4, 4, 4, 4}
+	if !bytes.Equal(file3Content, expectedFile3Content) {
+		t.Fatalf("Expected file 3 content: %v. Got: %v", expectedFile3Content, file3Content)
+	}
+
+	file4Content, err := ioutil.ReadFile(filepath.Join(path, "file4.txt"))
+	if err != nil {
+		t.Fatalf("Failed to read file content. Err: %s", err)
+	}
+	expectedFile4Content := []byte{5}
+	if !bytes.Equal(file4Content, expectedFile4Content) {
+		t.Fatalf("Expected file 4 content: %v. Got: %v", expectedFile4Content, file4Content)
+	}
+
+	file5Content, err := ioutil.ReadFile(filepath.Join(path, "dir1/file5.txt"))
+	if err != nil {
+		t.Fatalf("Failed to read file content. Err: %s", err)
+	}
+	expectedFile5Content := []byte{5, 5, 5, 5, 5}
+	if !bytes.Equal(file5Content, expectedFile5Content) {
+		t.Fatalf("Expected file 5 content: %v. Got: %v", expectedFile5Content, file5Content)
 	}
 }
 
